3c8db58cd5277d5a6fd7a8344946a10955934d6c
yinplus <liuzhenyin@126.com>
2018-11-27 17:02:19 +0800

    初始化项目路径

---
+++ src/com/liuzhenyin/HelloWorld.java
@@ -0,0 +1,10 @@
+package com.liuzhenyin;
+
+/**
+ * Created by 玉玺 on 2018/11/27
+ */
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("Hello World , this is a new growth porcess !");
+    }
+}
