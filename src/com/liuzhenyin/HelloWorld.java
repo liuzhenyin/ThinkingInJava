dd1d550a3f3f8a6aafa8382a7a1af4119ddcfffe
yinplus <liuzhenyin@126.com>
2018-11-27 18:50:22 +0800

    中文乱码

--- src/com/liuzhenyin/HelloWorld.java
+++ src/com/liuzhenyin/HelloWorld.java
@@ -1,10 +1,14 @@
 package com.liuzhenyin;
 
+import java.time.LocalDateTime;
+
 /**
- * Created by çŽ‰çŽº on 2018/11/27
+ * Created by yuxi on 2018/11/27
  */
 public class HelloWorld {
     public static void main(String[] args) {
-        System.out.println("Hello World , this is a new growth porcess !");
+        System.out.println("Hello World , this is a new growth porcess ! ÖÐÎÄ ");
+        LocalDateTime localDateTime = LocalDateTime.now();
+        System.out.println(localDateTime);
     }
 }
