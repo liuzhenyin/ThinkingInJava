b5fee656aecf70c816c5ae359e2f9ca0fe0c35ef
yinplus <liuzhenyin@126.com>
2018-11-28 08:47:27 +0800

    中文

--- src/com/liuzhenyin/HelloWorld.java
+++ src/com/liuzhenyin/HelloWorld.java
@@ -3,11 +3,11 @@ package com.liuzhenyin;
 import java.time.LocalDateTime;
 
 /**
- * Created by yuxi on 2018/11/27
+ * Created by ç‰çº on 2018/11/27
  */
 public class HelloWorld {
     public static void main(String[] args) {
-        System.out.println("Hello World , this is a new growth porcess ! ÖĞÎÄ ");
+        System.out.println("Hello æµå— , this is a new growth porcess ï¼");
         LocalDateTime localDateTime = LocalDateTime.now();
         System.out.println(localDateTime);
     }
