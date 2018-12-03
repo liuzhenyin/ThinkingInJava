06efcb7695c37fd00501e501850771b1f0aa35d3
liuzhenyin <liuzhenyin@126.com>
2018-12-03 20:27:37 +0800

    增加类

--- src/com/liuzhenyin/HelloWorld.java
+++ src/com/liuzhenyin/HelloWorld.java
@@ -1,6 +1,7 @@
 package com.liuzhenyin;
 
 import java.time.LocalDateTime;
+import java.util.Calendar;
 
 /**
  * Created by 玉玺 on 2018/11/27
@@ -10,5 +11,7 @@ public class HelloWorld {
         System.out.println("Hello 济南 , this is a new growth process ！");
         LocalDateTime localDateTime = LocalDateTime.now();
         System.out.println(localDateTime);
+        Calendar c=Calendar.getInstance();
+        System.out.println(Calendar.AM);
     }
 }
